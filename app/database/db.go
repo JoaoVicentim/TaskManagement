8531e1ba1c01686a6dbd414de232ea59d9a0b865
JoaoVicentim <joao.vicentim@avenue.us>
2024-11-21 09:40:22 -0300

    feat(database): implementar tentativas de conexão ao banco de dados

--- app/database/db.go
+++ app/database/db.go
@@ -3,6 +3,7 @@ package database
 import (
 	"TaskManagement/app/models"
 	"log"
+	"time"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -15,10 +16,19 @@ var (
 
 func DataBaseConnection() {
 	stringconnection := "host=postgres user=root password=root dbname=root port=5432 sslmode=disable"
-	DB, err = gorm.Open(postgres.Open(stringconnection))
-	if err != nil {
-		log.Panic("Falha ao conectar ao banco de dados")
+	for i := 0; i < 10; i++ {
+		DB, err = gorm.Open(postgres.Open(stringconnection))
+		if err == nil {
+			log.Println("Conexão com o banco de dados estabelecida com sucesso!")
+			break
+		}
+		log.Printf("Falha ao conectar ao banco de dados, tentativa %d: %v", i+1, err)
+		time.Sleep(2 * time.Second) // Espera 2 segundos antes da próxima tentativa
 	}
+	if err != nil {
+		log.Panic("Não foi possível conectar ao banco de dados após 10 tentativas.")
+	}
+
 	// Criar uma tabela no banco de dados com base no modelo de Tarefa
 	DB.AutoMigrate(&models.Task{})
 }
